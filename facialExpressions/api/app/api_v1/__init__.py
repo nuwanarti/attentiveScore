b38142c4bb7f35536bc66fca2e02ca91570fa1f7
nuwanarti <nuwan.t.attygalle@gmail.com>
2020-08-09 20:54:02 +0200

    body lang web and api

--- facialExpressions/api/app/api_v1/__init__.py
+++ facialExpressions/api/app/api_v1/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask_restplus import Api
-
+from flask_cors import CORS, cross_origin
 
 api_v1 = Blueprint('api_v1', __name__)
-api = Api(api_v1)
 
+api = Api(api_v1)
+# cors = CORS(api)
+# api.config['CORS_HEADERS'] = 'Content-Type'
 
 from . import routes
 
--- facialExpressions/api/app/api_v1/routes.py
+++ facialExpressions/api/app/api_v1/routes.py
@@ -1,6 +1,8 @@
 from flask_restplus import Resource
 from flask import Flask, jsonify, request
 import json
+from PIL import Image
+import cv2
 # import pandas as pd
 from . import api
 
@@ -34,15 +36,34 @@ class RegressionOut(Resource):
             "probability": 0.75
         }, 200
 
+class BodyLang(Resource):
+    def post(self):
+        print('hi there')
+        json_data = request.get_json(force=True)
+        images = json_data['images']
+        imgArray = []
+        for img in images:
+            imgdata = base64.b64decode(str(img))
+            m = Image.open(io.BytesIO(imgdata))
+            imgArray.append(cv2.cvtColor(np.array(m), cv2.COLOR_BGR2RGB))
+        print(imgArray)
+        # value = 
+        return {
+            "status": "done"
+        }, 200, {'Access-Control-Allow-Origin': '*'}
 
 class DB(Resource):
     def get(self):
+        # print('came here')
         return {
             "db": "Database here"
         }, 200
 
+# from flask_cors import CORS, cross_origin
+# cors = CORS(api)
+# api.config['CORS_HEADERS'] = 'Content-Type'
 
 api.add_resource(Status, '/status')
 api.add_resource(RegressionOut, '/regression')
 api.add_resource(DB, '/db')
-
+api.add_resource(BodyLang, '/bodyLang')
